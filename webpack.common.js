ff8c909720048fc5a2bd3ee4548d508d90386c6d
mgyang53 <mg960503@gmail.com>
2024-04-28 14:49:19 +0900

    [core] add favicon

--- webpack.common.js
+++ webpack.common.js
@@ -1,31 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const distDir = path.resolve(__dirname, 'dist');
+const srcDir = path.resolve(__dirname, 'src');
+const staticDir = path.resolve(__dirname, 'static');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: distDir,
     clean: true,
   },
   resolve: {
+    alias: {
+      '@': srcDir,
+    },
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
   module: {
     rules: [
       {
-        test: /\.(js|ts|tsx)$/i,
+        test: /\.(jsx|ts|tsx)$/i,
         exclude: /(node_modules)/,
         use: {
           loader: 'swc-loader',
         },
       },
+      {
+        test: /\.(png|jpe?g|svg|gif|webp)$/i,
+        type: 'asset/resource', // type을 통해 Webpack에 내장된 로더(Builtin Loader) 명시
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: 'public/index.html',
+      hash: true,
+      favicon: `${staticDir}/favicon.ico`,
     }),
   ],
   devtool: 'inline-source-map',
