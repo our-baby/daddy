aa6c71ca879e7ebd7bcc285c14eed6f757cd9b50
mgyang53 <mg960503@gmail.com>
2024-08-20 16:51:28 +0900

    feature : zustand 설치 및 auth, user 스토어 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import Routes from './Routes';
+import useUserStore from './store/UserStore';
+import useAuthStore from './store/AuthStore';
 
 function App() {
+  const { accessToken } = useAuthStore();
+
+  // 멤버 api 조회 parameter 얻은 후 작업 예정
+  if (accessToken) {
+    const user = useUserStore.getState();
+    console.log(user);
+    if (!user.id) {
+      // const response = await axiosInstance.post(`/members/${seq}`, params, {
+      //   headers: {
+      //     'certificated-token': accessToken,
+      //   },
+      // });
+    }
+  }
+
   return <Routes />;
 }
 
--- src/types/commonResponse.ts
+++ src/types/commonResponse.ts
@@ -1,8 +1,6 @@
 // 서버에서 내려주는 응답 구조
 export interface APIResponse<T> {
-  //   statusCode: number; // 상태코드 (보인 서버상태코드)
-  //   errorCode: number; // 에러코드 (본인 서버에러코드)
-  //   message: string; // 메시지
+  status: boolean; // 응답 상태
+  message?: string; // 메시지
   result: T; // 데이터 내용
-  //   timestamp: Date; // 시간
 }
